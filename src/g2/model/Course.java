ad237f98a6c64c970f1653783baf424035847ffe
Jose Rodriguez <joseocala@gmail.com>
2012-10-27 15:18:57 -0400

    Added course object.

---
+++ src/g2/model/Course.java
@@ -0,0 +1,16 @@
+package g2.model;
+
+import java.util.Map;
+
+public class Course {
+	
+	private String id;
+	private String rawHtml;
+	
+	private Map<String, Course> prereqs;
+	
+	public Course() {
+		
+	}
+
+}
